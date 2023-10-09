6bc5fe183c39f559fefad7b4207f3f8bb67f2781
Fauqs <yayano_1569_chino@hotmail.com>
2023-10-09 12:40:27 -0600

    Maximum algorithm

---
+++ max.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+#Max algorithm
+#By: yayano_1569_chino@hotmail.com
+#LICENSE GNU/GPL
+
+data=[1.0,3.14,6.2,0.1,5.3]
+maxi=-99999.99
+for x in data:
+    if x>maxi:
+        maxi=x
+print(maxi)
